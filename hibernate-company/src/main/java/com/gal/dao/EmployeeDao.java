6ef3007a45f0d2bd7eb2bc9ea8772ba98168d430
Shivam01080 <154500333+Shivam01080@users.noreply.github.com>
2026-03-10 19:03:39 +0530

    Add files via upload

---
+++ hibernate-company/src/main/java/com/gal/dao/EmployeeDao.java
@@ -0,0 +1,20 @@
+package com.gal.dao;
+
+import com.gal.model.Employee;
+import com.gal.hibernate_company.EntityManagerFactoryProvider;
+import jakarta.persistence.EntityManager;
+
+public class EmployeeDao {
+
+    public Employee getEmployee(int id) {
+
+        EntityManager em =
+                EntityManagerFactoryProvider.getEntityManager();
+
+        Employee employee = em.find(Employee.class, id);
+
+        em.close();
+
+        return employee;
+    }
+}
